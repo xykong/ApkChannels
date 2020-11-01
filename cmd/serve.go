9948237175357d97e397c5a7fe08035fb963aeb4
xykong <xy.kong@gmail.com>
2020-11-01 23:45:24 +0800

    add gin service

--- cmd/serve.go
+++ cmd/serve.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/xykong/ApkChannels/services"
 
 	"github.com/spf13/cobra"
 )
@@ -33,6 +34,8 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("serve called")
+
+		services.Start()
 	},
 }
 
---
+++ services/services.go
@@ -0,0 +1,13 @@
+package services
+
+import "github.com/gin-gonic/gin"
+
+func Start() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	_ = r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
